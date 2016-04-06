72631e21a17369cb724ef56685c8d2ed974b63cd
Kenneth Peters <kpeters13@uco.edu>
2016-04-06 18:44:40 -0500

    Committing GreenTower

---
+++ HighSens/src/com/highsens/game/tower/GreenTower.java
@@ -0,0 +1,176 @@
+package com.highsens.game.tower;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
+
+import com.highsens.game.GameData;
+import com.highsens.game.GameFigure;
+import com.highsens.game.Monster;
+import com.highsens.game.Tower;
+
+public class GreenTower extends AbstractTower implements Tower, GameFigure {
+
+	float x, y;
+	Point2D.Float target;
+	private int state = STATE_IDLE;
+
+	private String imagePath = System.getProperty("user.dir");
+	private String separator = System.getProperty("file.separator");
+
+	Monster m;
+	GameData gd;
+	private GameFigure currentTarget;
+
+	public GreenTower(float x, float y, GameData gd) {
+		this.gd = gd;
+		this.x = x;
+		this.y = y;
+		this.bulletCount = 1;
+		String imagePath = System.getProperty("user.dir");
+		String separator = System.getProperty("file.separator");
+		towerImage = getImage(imagePath + separator + "images" + separator + "GreenTower.png");
+		boundingBox = new BoundingBox((int) x, (int) x + 50, (int) y, (int) y + 69);
+		range = 200;
+		radius = new Ellipse2D.Double((this.x + 50) - 175, (this.y + 69) - 175, 300, 300);
+	}
+
+	@Override
+	public Image getImage(String fileName) {
+		Image image = null;
+		try {
+			image = ImageIO.read(new File(fileName));
+		} catch (Exception ioe) {
+			System.out.println("Error: Cannot open image:" + fileName);
+			JOptionPane.showMessageDialog(null, "Error: Cannot open image:" + fileName);
+		}
+		return image;
+	}
+
+	@Override
+	public float getXofMissileShoot() {
+		return x + 30;
+	}
+
+	@Override
+	public float getYofMissileShoot() {
+		return y + 20;
+	}
+
+	@Override
+	public void render(Graphics g) {
+		drawRange(g, x, y);
+		g.drawImage(towerImage, (int) x, (int) y, null);
+	}
+
+	@Override
+	public void updateState() {
+
+	}
+
+	@Override
+	public void update() {
+		if (currentTarget != null && (currentTarget.getState() == STATE_DONE || !this.collision(currentTarget))) {
+			currentTarget = null;
+		}
+		switch (state) {
+
+		case STATE_DONE:
+			gd.moneyManager("sellBlueTower", gd.getMoney());
+			break;
+		}
+	}
+
+	@Override
+	public void setState(int state) {
+		this.state = state;
+	}
+
+	@Override
+	public int getState() {
+		return state;
+	}
+
+	@Override
+	public void updateHealth() {
+
+	}
+
+	@Override
+	public boolean collision(GameFigure m) {
+		boolean collision = false;
+		if (radius.intersects(m.getX(), m.getY(), 47, 37)) {
+			collision = true;
+		} else {
+			collision = false;
+		}
+		return collision;
+	}
+
+	@Override
+	public int getHealth() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public double getX() {
+		return x;
+	}
+
+	@Override
+	public double getY() {
+		return y;
+	}
+
+	public void setLevel(int level) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean contains(float f, float g) {
+		return false;
+	}
+
+	public GameFigure getCurrentTarget() {
+		return currentTarget;
+	}
+
+	public void setCurrentTarget(GameFigure currentTarget) {
+		this.currentTarget = currentTarget;
+	}
+
+	@Override
+	public void setIsAngry(boolean t) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public boolean getIsAngry() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Image getUpgradeTowerImage() {
+		return getImage(imagePath + separator + "images" + separator + "GreenTowerUpgrade.png");
+
+	}
+}
